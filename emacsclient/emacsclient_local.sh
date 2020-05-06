65eaa21637539a5bba1a58023f5c74fba4eb2395
Dmitriy Kapustin <deff.dzehv@gmail.com>
2020-05-06 16:48:42 +0300

    find-file-literally alternative 4 emacsclient wrapper, switches buffer to fundamental-mode

--- emacsclient/emacsclient_local.sh
+++ emacsclient/emacsclient_local.sh
@@ -4,17 +4,32 @@
 # Works by connecting to the local Emacs-server and opens file in new buffer.
 # Allows to jump to line number
 
+# transform long options to short ones
+for arg in "$@"; do
+    shift
+    case "$arg" in
+        "--line")             set -- "$@" "-l" ;;
+        "--column")           set -- "$@" "-c" ;;
+        "--fundamental-mode") set -- "$@" "-f" ;;
+        *)                    set -- "$@" "$arg" ;;
+    esac
+done
+
 # A POSIX variable
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
 LINE=''
 COLUMN=''
+# to find file and set fundamental mode (like find-file-literally)
+FUNDAMENTAL_MODE=0
 
-while getopts "l:c:" opt; do
+while getopts "fl:c:" opt; do
     case $opt in
         l) LINE=$OPTARG
            ;;
         c) COLUMN=$OPTARG
+           ;;
+        f) FUNDAMENTAL_MODE=1
            ;;
         *) echo "No reasonable options found!"
            exit 1
@@ -34,8 +49,13 @@ elif [ $LINE ]; then
     LINE_ARG="+$LINE"
 fi
 
-if [ $LINE_ARG ]; then
+if [ "$FUNDAMENTAL_MODE" -eq 1 ]; then
+    # only 1 file
+    emacsclient -n -s ~/.emacs.d/server/server -e "(progn (find-file \"$1\") (fundamental-mode))"
+elif [ $LINE_ARG ]; then
+    # also only 1 file
     emacsclient -n -s ~/.emacs.d/server/server $LINE_ARG "$1"
 else
+    # can be multiple files
     emacsclient -n -s ~/.emacs.d/server/server "$@"
 fi
